3e10eb7d6e57b086af9ff3563594637aafe42358
TauriRe <42835552+TauriRe@users.noreply.github.com>
2024-01-29 12:05:24 +0200

    Add files via upload

---
+++ models/user.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  acts_as_token_authenticatable
+
+  # Include default devise modules. Others available are:
+  # :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :trackable, :confirmable, :lockable
+
+  has_many :passwords, dependent: :destroy
+  has_many :file_pushes, dependent: :destroy
+  has_many :urls, dependent: :destroy
+end
