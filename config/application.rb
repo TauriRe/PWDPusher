6d66e1e774dfa6fcf45e31677e236a0d5d26830b
TauriRe <42835552+TauriRe@users.noreply.github.com>
2024-01-29 12:19:26 +0200

    Add files via upload

---
+++ config/application.rb
@@ -0,0 +1,35 @@
+# rubocop:disable Rails/Output
+# frozen_string_literal: true
+
+require_relative 'boot'
+
+require 'rails/all'
+require 'version'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module PasswordPusher
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.1
+    config.active_support.cache_format_version = 7.1
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+    puts "Password Pusher Version: #{Version.current}"
+  end
+end
+
+# rubocop:enable Rails/Output
